b226a508c9c03450bf9bc7975f56d148d65d1c4e
mbiga <mbigaram.mb@gmail.com>
2023-03-01 16:43:10 -0300

    pagina de login

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Login from "../src/page/loginPage/Login"
+
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/loginPage/Login.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import imgLogo from "../../assets/img/imgLogo.png"
+import "./styles.css"
+import { ChakraProvider } from '@chakra-ui/react'
+import {
+    Flex,
+    Box,
+    FormControl,
+    Input,
+    Stack,
+} from '@chakra-ui/react';
+
+
+const Login = () => {
+    return (
+        <div>
+            <div className='DivLogin'>
+                <img className="img" src={imgLogo} alt="Logo img" />
+                <p className="labeddit">LabEddit</p>
+                <p className="paragrafo">O projeto de rede social da Labenu</p>
+            </div>
+            <ChakraProvider>
+                <Flex
+                    justify={'center'}
+                >
+                    <Stack >
+                        <Box
+                            w='420px'
+                            rounded={'lg'}
+                            p={8}>
+                            <Stack spacing={55}>
+                                <Stack spacing={1}>
+                                    <FormControl id="email">
+                                        <Input
+                                            p={26}
+                                            borderRadius={"4px"}
+                                            type="email" placeholder="E-mail" />
+                                    </FormControl>
+                                    <FormControl id="password">
+                                        <Input
+                                            p={26}
+                                            borderRadius={"4px"}
+                                            type="password" placeholder="Senha" />
+                                    </FormControl>
+                                </Stack>
+
+                                <Stack
+                                    spacing={4}>
+                                    <Box
+                                        as='button'
+                                        fontWeight='semibold'
+                                        fontSize={20}
+                                        p={3}
+                                        color='white'
+                                        borderRadius='30'
+                                        bgGradient='linear(to-r, #FF6787, #FAB050)'
+                                    >
+                                        Continuar
+                                    </Box>
+                                    <hr
+                                        className="hr" />
+
+                                    <Box
+                                        as='button'
+                                        p={3}
+                                        fontWeight='bold'
+                                        fontSize={20}
+                                        color='#FE7E02'
+                                        borderRadius='30'
+                                        border='1px'
+                                        bg='white'
+                                    >
+                                        Crie uma conta!
+                                    </Box>
+                                </Stack>
+                            </Stack>
+                        </Box>
+                    </Stack>
+                </Flex>
+            </ChakraProvider>
+
+        </div>
+
+    )
+}
+
+export default Login
